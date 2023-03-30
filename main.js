e8642ec0e5b5455f9c46c25aaea9e441608df743
Louis Jackson <louis.jackson@student.manchester.ac.uk>
2023-03-30 14:56:38 +0100

    Basic three js setup

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three.js";
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -15,10 +15,12 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
-    requestAnimationFrame( animate );
+	requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-    renderer.render( scene, camera );
-};
+	renderer.render( scene, camera );
+}
+
+animate();
